db173424953823c6bc410945220995d9544bb7f9
Elek Zoltán <e.zotya695@gmail.com>
2025-11-04 21:14:44 +0100

    implement course builder form

--- app/courses/new/page.tsx
+++ app/courses/new/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import CourseForm from "@/components/CourseForm";
+import React from "react";
 
 const NewCourse = () => {
   return (
-    <div>NewCourse</div>
-  )
-}
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Course Builder</h1>
 
-export default NewCourse
+        <CourseForm />
+      </article>
+    </main>
+  );
+};
+
+export default NewCourse;
