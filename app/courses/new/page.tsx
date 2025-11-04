5de3685dcb948e055b7bb629d1d82bca5a2b8a6f
Elek Zoltán <e.zotya695@gmail.com>
2025-11-04 22:06:17 +0100

    implemet authentication

--- app/courses/new/page.tsx
+++ app/courses/new/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NewCourse = () => {
   return (
-    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+    <main className="lg:w-1/3 md:w-2/3 items-center justify-center">
       <article className="w-full gap-4 flex flex-col">
         <h1>Course Builder</h1>
 
